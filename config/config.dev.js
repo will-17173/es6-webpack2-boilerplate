9770cd208fdcf1a0045cb3bd3302f1cce7dfbce6
will-17173 <huangzhaojian@gmail.com>
2017-07-31 16:27:06 +0800

    添加SSI支持

--- config/config.dev.js
+++ config/config.dev.js
@@ -3,6 +3,7 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./config.common');
 const autoprefixer = require('autoprefixer');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const SSICompileWebpackplugin = require('ssi-webpack-plugin');
 const postcssConfig = {
   loader: 'postcss-loader',
   options: {
@@ -61,7 +62,17 @@ const devConfig = webpackMerge(commonConfig, {
         NODE_ENV: "'development'"
       }
     }),
-    new DashboardPlugin()
+    new DashboardPlugin(),
+    new SSICompileWebpackplugin({
+        ext: '.html|.shtml',
+        publicPath: '',
+        localBaseDir: './views',
+        minify: false,
+        // remoteBasePath:"http://baidu.com",//如果传了此参数，则表示从远程http请求中获取ssi文件的内容 
+        variable:{
+            'QUERY_STRING':"test=1",//如果ssi文件地址里有引用变量,如${QUERY_STRING}，则会用此参数里的对应key的值替换 
+        }
+    })
   ],
 
   devServer: {
